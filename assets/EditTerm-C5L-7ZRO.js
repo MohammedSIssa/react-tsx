0c8bb35f1b6c4b7468801e9ac0ceb72a9617b09a
MohammedSIssa <mohammedsissa1999@gmail.com>
2025-12-21 12:52:12 +0200

    Updates

---
+++ assets/EditTerm-C5L-7ZRO.js
@@ -0,0 +1,1 @@
+import{u as e,j as r}from"./index-B6O7YQjh.js";function s(){const t=e();return r.jsxs("pre",{dir:"ltr",children:["Received Data: ",JSON.stringify(t.state,null,2)]})}export{s as default};
