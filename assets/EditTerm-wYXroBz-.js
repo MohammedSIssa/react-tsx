eacd41c314dfdcedc3276a6029c6d2b884a66818
MohammedSIssa <mohammedsissa1999@gmail.com>
2025-12-21 11:44:34 +0200

    Updates

--- assets/EditTerm-wYXroBz-.js
+++
@@ -1,1 +0,0 @@
-import{u as e,j as r}from"./index-WDlYV7QW.js";function s(){const t=e();return r.jsxs("pre",{dir:"ltr",children:["Received Data: ",JSON.stringify(t.state,null,2)]})}export{s as default};
---
+++ assets/EditVehicle-Cyij-7-j.js
@@ -0,0 +1,1 @@
+import{u as e,j as i}from"./index-CRgC32zz.js";function s(){const t=e();return i.jsxs("pre",{dir:"ltr",children:["Received Data: ",JSON.stringify(t.state,null,2)]})}export{s as default};
