63734b493dcb3b1389d1b8b1e83d3642f56ed5e8
MohammedSIssa <mohammedsissa1999@gmail.com>
2025-12-23 13:05:45 +0200

    Updates

---
+++ assets/EditVehicle-oFRgnCuf.js
@@ -0,0 +1,1 @@
+import{a as e,j as i}from"./index-CX8-0uuM.js";function s(){const t=e();return i.jsxs("pre",{dir:"ltr",children:["Received Data: ",JSON.stringify(t.state,null,2)]})}export{s as default};
