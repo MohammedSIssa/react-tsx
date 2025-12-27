5bfe328080008ba5ca47c1da7f3bdea1402a4526
MohammedSIssa <mohammedsissa1999@gmail.com>
2025-12-27 13:18:17 +0200

    Updates

--- assets/EditVehicle-jft_mQRh.js
+++
@@ -1,1 +0,0 @@
-import{b as e,j as i}from"./index-Dgw4cujx.js";function s(){const t=e();return i.jsxs("pre",{dir:"ltr",children:["Received Data: ",JSON.stringify(t.state,null,2)]})}export{s as default};
