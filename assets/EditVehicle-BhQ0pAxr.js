aca336124b2997fdd21bedac6fbc5557a99d3931
MohammedSIssa <mohammedsissa1999@gmail.com>
2025-12-27 13:04:45 +0200

    Updates

--- assets/EditVehicle-BhQ0pAxr.js
+++
@@ -1,1 +0,0 @@
-import{b as e,j as i}from"./index-DmrK3OXr.js";function s(){const t=e();return i.jsxs("pre",{dir:"ltr",children:["Received Data: ",JSON.stringify(t.state,null,2)]})}export{s as default};
