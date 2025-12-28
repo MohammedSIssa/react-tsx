f1d83f502bf537d99976040eaa4ff337ac1b9afc
MohammedSIssa <mohammedsissa1999@gmail.com>
2025-12-28 10:28:47 +0200

    Updates

---
+++ assets/EditTerm-DHPydb1s.js
@@ -0,0 +1,1 @@
+import{b as e,j as r}from"./index-DHZZGHf3.js";function s(){const t=e();return r.jsxs("pre",{dir:"ltr",children:["Received Data: ",JSON.stringify(t.state,null,2)]})}export{s as default};
--- assets/EditTerm-wiKmxZam.js
+++
@@ -1,1 +0,0 @@
-import{b as e,j as r}from"./index-i3f7qJO0.js";function s(){const t=e();return r.jsxs("pre",{dir:"ltr",children:["Received Data: ",JSON.stringify(t.state,null,2)]})}export{s as default};
