c79dab33f2a20297b09ec3c6856cb75343415468
MohammedSIssa <mohammedsissa1999@gmail.com>
2025-12-28 14:14:28 +0200

    Updates

--- assets/EditVehicle-ClePwxtG.js
+++
@@ -1,1 +0,0 @@
-import{b as e,j as i}from"./index-DkPkl7yF.js";function s(){const t=e();return i.jsxs("pre",{dir:"ltr",children:["Received Data: ",JSON.stringify(t.state,null,2)]})}export{s as default};
