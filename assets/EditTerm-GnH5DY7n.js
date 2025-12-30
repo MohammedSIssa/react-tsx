4fc2abf0f3b77163c8627178cb45bc60f58a60ce
MohammedSIssa <mohammedsissa1999@gmail.com>
2025-12-30 12:49:25 +0200

    Updates

---
+++ assets/EditTerm-GnH5DY7n.js
@@ -0,0 +1,1 @@
+import{b as e,j as r}from"./index-DbNO7JWG.js";function s(){const t=e();return r.jsxs("pre",{dir:"ltr",children:["Received Data: ",JSON.stringify(t.state,null,2)]})}export{s as default};
